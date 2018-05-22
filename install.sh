2423b6e899135e34217743ab0ce16503044a1fbb
Pavel Nikitin <p.doomkin@ya.ru>
2018-05-22 11:08:58 +0300

    Начальное состояние

---
+++ install.sh
@@ -0,0 +1,7 @@
+DIR=/usr/share/backgrounds/day-cycle
+mkdir -p $DIR
+cp -f backgrounds/1_morning.jpg $DIR
+cp -f backgrounds/3_day.jpg $DIR
+cp -f backgrounds/5_night.jpg $DIR
+cp -f day-cycle-backgrounds.xml $DIR
+cp -f day-cycle-theme.xml /usr/share/gnome-background-properties
